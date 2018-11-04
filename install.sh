849431b4d1186d11a76c34ec2f323efdbea7e2b9
mifurm <michal.furmankiewicz@hotmail.com>
2018-11-04 12:33:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,3 @@ sudo apt-get install apache2 lynx --assume-yes
 sudo apt-get install python3.6 --assume-yes
 sudo apt install python3-pip --assume-yes
 sudo add-apt-repository ppa:eugenesan/ppa --assume-yes
-sudo apt-get install mc --assume-yes
