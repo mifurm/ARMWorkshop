6876d851ae6f6ff0f1955959256ec872a5a37161
mifurm <michal.furmankiewicz@hotmail.com>
2019-01-11 23:34:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 sudo apt-get update --assume-yes
-sudo apt-get install apache2 lynx --assume-yes
-sudo apt-get install python3.6 --assume-yes
-sudo apt install python3-pip --assume-yes
+sudo apt-get upgrade --assume-yes
