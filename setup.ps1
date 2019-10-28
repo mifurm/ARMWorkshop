45b0999b7bce1698dd4bd4a0a0b1fc3508651ecb
mifurm <michal.furmankiewicz@hotmail.com>
2019-10-28 06:01:41 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,40 +1,33 @@
-Install-Module AzureAD
-Connect-AzureAD
+#Install-Module AzureAD
+#Connect-AzureAD
  
-$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
-$PasswordProfile.Password = "bardzoskomplikowanehaslo123!@#"
+#$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+#$PasswordProfile.Password = "bardzoskomplikowanehaslo123!@#"
  
 #id pierwszego użytkownika
-$startCounter=1
-$counter=$startCounter
+#$startCounter=1
+#$counter=$startCounter
 #konwencja nazewnicza dla Twoich użytkowników
-$userName = "student"+$counter
+#$userName = "student"+$counter
 #UPN. Uważaj: sprawdź, czy taki UPN istnieje w Twojej domenie w Azure AD. 
-$upn = $userName +"@chmurowiskolab.onmicrosoft.com"
+#$upn = $userName +"@mbankk8s.onmicrosoft.com"
 #konwencja nazewnicza dla Twoich grup zasobów
-$rgName ="st-rg"+$counter
+#$rgName ="st-rg"+$counter
 #maksymalna liczba użytkowników 
-$nrUsers = 2
-$maxUsers = $startCounter + $nrUsers
+#$nrUsers = 2
+#$maxUsers = $startCounter + $nrUsers
 
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
 $PasswordProfile.Password = "Testtest123!@#"
 $counter=1
-$nrUsers = 15
+$nrUsers = 20
 $maxUsers = $startCounter + $nrUsers
 for (;$counter -le $maxUsers; $counter++)
 {
     $userName = "student"+$counter
-    $upn = $userName +"@chmurowiskolab.onmicrosoft.com"
+    $upn = $userName +"@<TWOJA DOMENA>.onmicrosoft.com"
     $rgName ="st-rg"+$counter
     New-AzureADUser -DisplayName $userName -PasswordProfile $PasswordProfile -UserPrincipalName $upn -AccountEnabled $true -MailNickName $userName
-}
-
-
-$counter=1
-$nrUsers = 15
-$maxUsers = $startCounter + $nrUsers
-for (;$counter -le $maxUsers; $counter++) { New-AzureRmResourceGroup -Name $rgName -Location 'North Europe' }
-
-#New-AzureRmRoleAssignment -SignInName $upn ` -RoleDefinitionName "Contributor" -ResourceGroupName $rgName
+    New-AzureRmResourceGroup -Name $rgName -Location 'West Europe'
+    New-AzureRmRoleAssignment -SignInName $upn ` -RoleDefinitionName "Contributor" -ResourceGroupName $rgName
 }
