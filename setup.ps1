a5bb3f925f0c35d17709b4c39fb18371e2125688
mifurm <michal.furmankiewicz@hotmail.com>
2019-12-02 17:01:46 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,22 +1,3 @@
-#Install-Module AzureAD
-#Connect-AzureAD
- 
-#$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
-#$PasswordProfile.Password = "bardzoskomplikowanehaslo123!@#"
- 
-#id pierwszego użytkownika
-#$startCounter=1
-#$counter=$startCounter
-#konwencja nazewnicza dla Twoich użytkowników
-#$userName = "student"+$counter
-#UPN. Uważaj: sprawdź, czy taki UPN istnieje w Twojej domenie w Azure AD. 
-#$upn = $userName +"@mbankk8s.onmicrosoft.com"
-#konwencja nazewnicza dla Twoich grup zasobów
-#$rgName ="st-rg"+$counter
-#maksymalna liczba użytkowników 
-#$nrUsers = 2
-#$maxUsers = $startCounter + $nrUsers
-
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
 $PasswordProfile.Password = "Testtest123!@#"
 $counter=1
