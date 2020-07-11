158ed98448eacfa9cb87761441aee23292c50e10
mifurm <michal.furmankiewicz@hotmail.com>
2020-07-11 11:59:43 +0200

    Update deleteWorkshop.ps1
    add az cli line

--- deleteWorkshop.ps1
+++ deleteWorkshop.ps1
@@ -7,3 +7,8 @@ for (;$counter -le $maxUsers; $counter++)
     $rgName ="sec-st-rg"+$counter
     Remove-AzureRmResourceGroup -Name $rgName -Force -AsJob
 }
+
+#
+#
+# az group list --query "[?starts_with(name, 'stk8srg')].[name]" --output tsv | xargs -n 1 bash -c 'az group delete -n $0 --yes --no-wait'
+#
