946505df6f89d9db2aa00a68fed4114b9e3c9ae7
mifurm <michal.furmankiewicz@hotmail.com>
2021-08-01 12:33:50 +0200

    Update connectToAzureADwithMSI.ps1

--- connectToAzureADwithMSI.ps1
+++ connectToAzureADwithMSI.ps1
@@ -1,9 +1,4 @@
-Connect-azaccount -identity
+Connect-AzAccount -Identity
 $context = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile.DefaultContext
-$graphToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, "https://graph.microsoft.com").AccessToken
 $aadToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, "https://graph.windows.net").AccessToken
-
-Write-Output "Hi I'm $($context.Account.Id)"
-
-
 Connect-AzureAD -AadAccessToken $aadToken -AccountId $context.Account.Id -TenantId $context.tenant.id
