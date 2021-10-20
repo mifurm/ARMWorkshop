153c7c644ff15d4de43d326a2fdb16000c17928b
mifurm <michal.furmankiewicz@hotmail.com>
2021-10-20 06:18:49 +0200

    fixes

--- postgresql-downloadlogs.bash
+++ postgresql-downloadlogs.bash
@@ -1,1 +1,4 @@
-az postgres server-logs list -g [resource_group] -s [server-name] --query '[].{Name:name}' -o tsv |xargs -I {} az postgres server-logs download -g [resource_group] -s [server-name] -n {}
+#
+#
+#
+az postgres server-logs list -g [resource_group] -s [server-name] --query '[].{Name:name}' -o tsv | xargs -I {} az postgres server-logs download -g [resource_group] -s [server-name] -n {}
